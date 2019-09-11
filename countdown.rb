498a655ec498fcafe5cc27e5b2a4516111ca73f8
Zachary Schulz <zjschulz@gmail.com>
2019-09-11 23:26:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(number)
     return "HAPPY NEW YEAR!"
   end
 end
+
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  if number == 0
+    return "HAPPY NEW YEAR!"
+  end
+  sleep(5)
+end
